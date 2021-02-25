b2ce7c1d7abec8f9b509a516e63c6e47025cc142
John Doe <kurbanof.atabek@gmail.com>
2021-02-25 11:40:00 +0600

    first commit

---
+++ js/nigara.js
@@ -0,0 +1,12 @@
+$('.burger-menu').on('click', function (e) {
+  $('.burger-menu').toggleClass('is-active');
+  $('.nav').toggleClass('is-active');
+});
+
+// const burgerMenu = document.querySelector('.burger-menu');
+// const nav = document.querySelector('.nav');
+
+// burgerMenu.addEventListener('mouseenter', () => {
+//   burgerMenu.classList.toggle('is-active');
+//   nav.classList.toggle('is-active');
+// });
